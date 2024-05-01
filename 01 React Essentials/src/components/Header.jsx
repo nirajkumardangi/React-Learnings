95ea8fa759340d3660cc2d72e76fb846282533b2
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-05-01 14:55:18 +0530

    Alternative props syntax,passing props through data.js file [core concept]

--- 01 React Essentials/src/components/Header.jsx
+++ 01 React Essentials/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import reactImg from '../assets/react-core-concepts.png';
-import componentsImg from '../assets/components.png';
-import CoreConcept from "../components/CoreConcept";
+import CoreConcept from './CoreConcept';
+import { CORE_CONCEPTS } from '../data.js';
 
 const reactDescriptions = ["Fundamentals", "Crucial", "Core"];
 
@@ -27,13 +27,25 @@ function Header() {
           <h2>Time to get started!</h2>
           <ul>
             <CoreConcept 
-              title="components" 
-              description = "The core UI building block"
-              image = {componentsImg}
+              title={CORE_CONCEPTS[0].title} 
+              description = {CORE_CONCEPTS[0].description}
+              image = {CORE_CONCEPTS[0].image}
             />
-            <CoreConcept />
-            <CoreConcept />
-            <CoreConcept />
+            <CoreConcept 
+              title={CORE_CONCEPTS[1].title} 
+              description = {CORE_CONCEPTS[1].description}
+              image = {CORE_CONCEPTS[1].image}
+            />
+            <CoreConcept 
+              title={CORE_CONCEPTS[2].title} 
+              description = {CORE_CONCEPTS[2].description}
+              image = {CORE_CONCEPTS[2].image}
+            />
+            <CoreConcept 
+              title={CORE_CONCEPTS[3].title} 
+              description = {CORE_CONCEPTS[3].description}
+              image = {CORE_CONCEPTS[3].image}
+            />
           </ul>
         </section>
       </main>
