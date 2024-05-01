f727e5463f9298095f4083fa2331d41f4f07c812
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-05-01 16:34:54 +0530

    Seperate files, Improve project structure

--- 01 React Essentials/src/App.jsx
+++ 01 React Essentials/src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/Header";
+import Header from "./components/Header/Header.jsx";
 import CoreConcept from './components/CoreConcept';
 import { CORE_CONCEPTS } from './data.js';
 
