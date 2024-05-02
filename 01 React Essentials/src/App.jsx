6628b6d0976e9ea2d2b1c3e1dc3199ed85661f89
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-05-02 14:33:44 +0530

    Outputing list data dynamically

--- 01 React Essentials/src/App.jsx
+++ 01 React Essentials/src/App.jsx
@@ -35,10 +35,9 @@ function App() {
         <section id='core-concepts'>
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((conceptItem) => (
+              <CoreConcept {...conceptItem}/>
+              ))}
           </ul>
         </section>
         <section id="examples">
