fba2829c055de7207a226e4208837f1195c4d5f5
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-05-05 06:02:43 +0530

    First Step Toward Our Tic-Tac-Toe Project

--- 02-Tic-Tac-Toe/src/App.jsx
+++ 02-Tic-Tac-Toe/src/App.jsx
@@ -1,9 +1,28 @@
-
+import './index.css'
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player">
+              Player 1
+              <span className="player-symbol">X</span>
+            </span>
+            <button>Edit</button>
+          </li>
+          <li>
+            <span className="player">
+              Player 2
+              <span className="player-symbol">0</span>
+            </span>
+            <button>Edit</button>
+          </li>
+        </ol>
+        GAME BAR
+      </div>
+      LOGS
+    </main>
   )
 }
 
