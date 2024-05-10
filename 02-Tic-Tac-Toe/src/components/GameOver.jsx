50a13a95ce73cd2e6597a233d7e475402a9fd4b0
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-05-10 18:46:45 +0530

    CSS: Making Mobile Responsive

--- 02-Tic-Tac-Toe/src/components/GameOver.jsx
+++ 02-Tic-Tac-Toe/src/components/GameOver.jsx
@@ -2,8 +2,8 @@ export default function GameOver({ winner, onRestart }) {
   return (
     <div id="game-over">
       <h2>Game Over!</h2>
-      {winner && <p>{winner} won!</p>}
-      {!winner && <p>It's a draw!</p>}
+      {winner && <p>{winner} Victory ! 🎉</p>}
+      {!winner && <p>Game Draw ! 🤗 </p>}
       <p><button onClick={onRestart}>Rematch!</button></p>
     </div>
   )
