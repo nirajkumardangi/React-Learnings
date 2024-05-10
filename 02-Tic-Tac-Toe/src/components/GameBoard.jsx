3c9f79a7eb99157fb3ac1a2a2f99eaa3bbcebf9a
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-05-10 07:38:52 +0530

    Disable Multiple Button Click

--- 02-Tic-Tac-Toe/src/components/GameBoard.jsx
+++ 02-Tic-Tac-Toe/src/components/GameBoard.jsx
@@ -8,8 +8,8 @@ export default function GameBoard({ onSelectSquare, turns }) {
   let gameBoard = initialGameBoard;
 
   for (const turn of turns) {
-    const {square, player} = turn;
-    const {row, col} = square;
+    const { square, player } = turn;
+    const { row, col } = square;
     gameBoard[row][col] = player;
   }
 
@@ -32,7 +32,12 @@ export default function GameBoard({ onSelectSquare, turns }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                <button onClick={() =>
+                  onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
