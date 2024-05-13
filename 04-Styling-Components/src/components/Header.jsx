79a565dca39bb6e32f007c0e8e018bd28982839a
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-05-13 12:56:09 +0530

    Inline CSS is Scoped to Components!

--- 04-Styling-Components/src/components/Header.jsx
+++ 04-Styling-Components/src/components/Header.jsx
@@ -6,7 +6,9 @@ export default function Header() {
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <p style={{
+        color: 'red',
+      }}>A community of artists and art-lovers.</p>
     </header>
   );
 }
