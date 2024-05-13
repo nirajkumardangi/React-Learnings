3ec0aaf57e02f82ae09d004f27557305b4e99521
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-05-13 13:49:43 +0530

    Applying Dynamic & Conditional Styling with CSS Classes

--- 04-Styling-Components/src/components/AuthInputs.jsx
+++ 04-Styling-Components/src/components/AuthInputs.jsx
@@ -24,18 +24,18 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <div className="controls">
         <p>
-          <label>Email</label>
+          <label className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</label>
           <input
-            type="email"
-            style={{
-              backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
-            }}
-            // className={emailNotValid ? 'invalid' : undefined}
+            // type="email"
+            // style={{
+            //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
+            // }}
+            className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <label>Password</label>
+          <label className={`label ${passwordNotValid ? 'invalid' : ''}`}>Password</label>
           <input
             type="password"
             className={passwordNotValid ? 'invalid' : undefined}
--- 04-Styling-Components/src/components/Header.jsx
+++ 04-Styling-Components/src/components/Header.jsx
@@ -6,9 +6,7 @@ export default function Header() {
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
-      <p style={{
-        color: 'red',
-      }}>A community of artists and art-lovers.</p>
+      <p>A community of artists and art-lovers.</p>
     </header>
   );
 }
