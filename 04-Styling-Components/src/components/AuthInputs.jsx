b615cd37c704f3c1e8472ba0203f4cb8dfa86f03
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-05-13 17:40:15 +0530

    Dynamically Styling with Styled Components

--- 04-Styling-Components/src/components/AuthInputs.jsx
+++ 04-Styling-Components/src/components/AuthInputs.jsx
@@ -15,16 +15,16 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: #6b7280;
+  color: ${({$invalid}) => $invalid ? '#f87171' : '#6b7180'};
 `
 
 const Input = styled.input`
   width: 100%;
   padding: 0.75rem 1rem;
   line-height: 1.5;
-  background-color: #d1d5db;
-  color: #374151;
-  border: 1px solid transparent;
+  background-color: ${({$invalid}) => $invalid ? '#fed2d2' : '#d1d5db'};
+  color: ${({$invalid}) => $invalid ? '#ef4444' : '#374151'};
+  border: 1px solid ${({$invalid}) => $invalid ? '#f73f3f' : 'transparent'};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `
@@ -53,21 +53,20 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlContainer>
         <p className='paragraph'>
-          <Label className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</Label>
+          <Label $invalid={emailNotValid}>Email</Label>
+          {/* because "invalid" is a default props so it shows warning on console, instead of default props we can use "$invalid" */}
           <Input
-            // type="email"
-            // style={{
-            //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
-            // }}
-            className={emailNotValid ? 'invalid' : undefined}
+            $invalid={emailNotValid}
+            type="email"
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
-          <Label className={`label ${passwordNotValid ? 'invalid' : ''}`}>Password</Label>
+          <Label $invalid={passwordNotValid}>Password</Label>
           <Input
+            $invalid={passwordNotValid}
             type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            className={passwordNotValid ? '$invalid' : undefined}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
