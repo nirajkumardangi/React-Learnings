4bac81d4c5c70d585e487cbab03819f84a0e7267
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-05-14 06:24:22 +0530

    Styled Components: Pseudo Selecter, Nested Rules & Media Queries

--- 04-Styling-Components/src/components/AuthInputs.jsx
+++ 04-Styling-Components/src/components/AuthInputs.jsx
@@ -27,7 +27,22 @@ const Input = styled.input`
   border: 1px solid ${({$invalid}) => $invalid ? '#f73f3f' : 'transparent'};
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
+`;
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+
+  &:hover {
+    background-color: #f0920e;
+  }
+`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -78,7 +93,7 @@ export default function AuthInputs() {
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
