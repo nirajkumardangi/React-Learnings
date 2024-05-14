b11efb54637d62b3baed4f83da6f054c8961440d
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-05-14 15:23:18 +0530

    FINISHED: Add All Styling with Tailwindin this Project

--- 05-Styling-with-Tailwind/src/components/AuthInputs.jsx
+++ 05-Styling-with-Tailwind/src/components/AuthInputs.jsx
@@ -24,8 +24,9 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <div>
+    <div id="auth-inputs" className="w-full max-w-sm p-8
+    mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className='flex flex-col gap-2 mb-6'>
           <Input
             label="Email"
             invalid={emailNotValid}
@@ -40,9 +41,9 @@ export default function AuthInputs() {
           />
       </div>
 
-      <div className="actions">
-        <button type="button" className="text-button">
-          Create a new account
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
+          Create a new text
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
       </div>
