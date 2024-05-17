139bdd5f6b1f5f5287dc57d866cee501b78e98ea
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-05-17 07:25:25 +0530

    Adding ResultModal Components & Forwarding ref

--- 06-Refs-and-Portals/src/components/TimerChallenge.jsx
+++ 06-Refs-and-Portals/src/components/TimerChallenge.jsx
@@ -1,37 +1,43 @@
-import { useState } from "react";
-
-let timer;
+import { useState, useRef } from "react";
+import ResultModal from "./ResultModal.jsx";
 
 export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+  const dialog = useRef();
 
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
   function handelStart() {
-    timer = setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
+      if (dialog.current) {
+        dialog.current.showModal();
+      }
     }, targetTime * 1000);
 
     setTimerStarted(true);
   }
 
   function handelStop() {
-    clearTimeout(timer);
+    clearTimeout(timer.current);
   }
 
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      {timerExpired ? <p>You Lost!</p> : ''}
-      <p className="challenge-time">
-        {targetTime} second{targetTime > 1 ? 's' : ''}
-      </p>
-      <p>
-        <button onClick={ timerStarted ? handelStop : handelStart}>{timerStarted ? 'Stop' : 'Start'} Challenge</button>
-      </p>
-      <p className="">
-        {timerStarted ? 'Time is Running...' : 'Timer Inactive'}
-      </p>
-    </section>
+    <>
+      {<ResultModal ref={dialog} targetTime={targetTime} result='lost' />}
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? 's' : ''}
+        </p>
+        <p>
+          <button onClick={timerStarted ? handelStop : handelStart}>{timerStarted ? 'Stop' : 'Start'} Challenge</button>
+        </p>
+        <p className="">
+          {timerStarted ? 'Time is Running...' : 'Timer Inactive'}
+        </p>
+      </section>
+    </>
   );
 }
