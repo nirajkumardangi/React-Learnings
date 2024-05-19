b5b3c3740cf7ca5b59fa5ce42b7f318efad8bea2
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-05-19 19:45:09 +0530

    Spliting add Adding  NoProjectSelected and ButtonComponents

--- 07-Project-Management/src/App.jsx
+++ 07-Project-Management/src/App.jsx
@@ -1,11 +1,14 @@
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
+
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
-      <NewProject />
+      <NoProjectSelected />
+      {/* <NewProject /> */}
     </main>
   );
 }
