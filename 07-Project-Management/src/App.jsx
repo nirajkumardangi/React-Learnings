73a8eb7050f525c4dcae2e125c78d121fdf789af
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-05-19 19:30:14 +0530

    Styling and Adding Buttons & Input Components

--- 07-Project-Management/src/App.jsx
+++ 07-Project-Management/src/App.jsx
@@ -1,10 +1,12 @@
+import NewProject from "./components/NewProject";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
   return (
-    <>
+    <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
-    </>
+      <NewProject />
+    </main>
   );
 }
 
--- 07-Project-Management/src/components/ProjectsSidebar.jsx
+++ 07-Project-Management/src/components/ProjectsSidebar.jsx
@@ -1,11 +1,9 @@
-
-
 export default function ProjectsSidebar() {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl h-screen">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Project</h2>
       <div>
-        <button className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600">
+        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
           + Add Project
         </button>
       </div>
