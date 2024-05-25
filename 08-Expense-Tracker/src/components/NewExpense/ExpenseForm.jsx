96999fb156c8e8c39842460e6e41ad334135f16b
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-05-25 13:07:13 +0530

    Handling Form Submission

--- 08-Expense-Tracker/src/components/NewExpense/ExpenseForm.jsx
+++ 08-Expense-Tracker/src/components/NewExpense/ExpenseForm.jsx
@@ -6,81 +6,46 @@ export default function ExpenseForm() {
   const [enteredAmount, setEnteredAmount] = useState();
   const [enteredDate, setEnteredDate] = useState();
 
-  // const [userInput, setUserInput] = useState({
-  //   enteredTitle: "",
-  //   enteredAmount: "",
-  //   enteredDate: "",
-  // });
-
-  // function titleChangeHandler(event) {
-  //   // setEnteredTitle(event.target.value)
-
-  //   // setUserInput({
-  //   //   ...userInput,
-  //   //   enteredTitle: event.target.value,
-  //   // });
-
-  //   setUserInput((prevState) => {
-  //     return {
-  //       ...prevState,
-  //       enteredTitle: event.target.value,
-  //     };
-  //   });
-  // }
-
-  // function amountChangeHandler(event) {
-  //   // setEnteredAmount(event.target.value);
-
-  //   // setUserInput({
-  //   //   ...userInput,
-  //   //   enteredAmount: event.target.value,
-  //   // });
-
-  //   setUserInput((prevState) => {
-  //     return {
-  //       ...prevState,
-  //       enteredAmount: event.target.value,
-  //     };
-  //   });
-  // }
-
-  // function dateChangeHandler(event) {
-  //   // setEnteredADate(event.target.value);
-
-  //   // setUserInput({
-  //   //   ...userInput,
-  //   //   enteredDate: event.target.value,
-  //   // });
-
-  //   setUserInput((prevState) => {
-  //     return {
-  //       ...prevState,
-  //       enteredDate: event.target.value,
-  //     };
-  //   });
-  // }
-
-  function inputChangeHandler(identifier, value) {
-    if (identifier === "title") {
-      setEnteredTitle(value);
-    } else if (identifier === 'amount') {
-      setEnteredAmount(value);
-    } else {
-      setEnteredDate(value);
-    }
+  function titleChangeHandler(event) {
+    setEnteredTitle(event.target.value);
   }
 
+  function amountChangeHandler(event) {
+    setEnteredAmount(event.target.value);
+  }
+
+  function dateChangeHandler(event) {
+    setEnteredDate(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    }
+
+    console.log(expenseData);
+  }
+
+  // function inputChangeHandler(identifier, value) {
+  //   if (identifier === "title") {
+  //     setEnteredTitle(value);
+  //   } else if (identifier === "amount") {
+  //     setEnteredAmount(value);
+  //   } else {
+  //     setEnteredDate(value);
+  //   }
+  // }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input
-            type="text"
-            onChange={(event) =>
-              inputChangeHandler("title", event.target.value)
-            }
-          />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -88,9 +53,7 @@ export default function ExpenseForm() {
             type="number"
             min="0.01"
             step="0.01"
-            onChange={(event) =>
-              inputChangeHandler("amount", event.target.value)
-            }
+            onChange={amountChangeHandler}
           />
         </div>
         <div className="new-expense__control">
@@ -99,9 +62,7 @@ export default function ExpenseForm() {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
-            onChange={(event) =>
-              inputChangeHandler("date", event.target.value)
-            }
+            onChange={dateChangeHandler}
           />
         </div>
       </div>
