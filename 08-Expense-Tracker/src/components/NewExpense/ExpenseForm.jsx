2710ae391c0aafafd1ccc873ba43c493739e766c
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-05-25 11:54:20 +0530

    Listning User Inputs Through useState Hook

--- 08-Expense-Tracker/src/components/NewExpense/ExpenseForm.jsx
+++ 08-Expense-Tracker/src/components/NewExpense/ExpenseForm.jsx
@@ -1,20 +1,41 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 
 export default function ExpenseForm() {
+  const [enteredTitle, setEnteredTitle] = useState();
+  const [enteredAmount, setEnteredAmount] = useState();
+  const [enteredDate, setEnteredDate] = useState();
+
+  function titleChangeHandler(e) {
+    setEnteredTitle(e.target.value);
+  }
+
+  function amountChangeHandler(e) {
+    setEnteredAmount(e.target.value);
+  }
+
+  function dateChangeHandler(e) {
+    setEnteredDate(e.target.value)
+  }
+
+  console.log(enteredAmount);
+  console.log(enteredTitle);
+  console.log(enteredDate);
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler}/>
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
         </div>
         <div className="new-expense__control">
-          <label>Title</label>
-          <input type="date" min="2019-01-01" max="2021-" />
+          <label>Date</label>
+          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
         </div>
       </div>
       <div className="new-expense__actions">
