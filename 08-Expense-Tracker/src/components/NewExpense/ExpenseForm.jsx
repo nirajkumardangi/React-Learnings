5681364d69536e549731a93f0dabe9affdbb9f07
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-05-25 12:12:06 +0530

    Updating State That Depends On The Previous State

--- 08-Expense-Tracker/src/components/NewExpense/ExpenseForm.jsx
+++ 08-Expense-Tracker/src/components/NewExpense/ExpenseForm.jsx
@@ -6,53 +6,83 @@ export default function ExpenseForm() {
   // const [enteredAmount, setEnteredAmount] = useState();
   // const [enteredDate, setEnteredDate] = useState();
   const [userInput, setUserInput] = useState({
-    enteredTitle: '',
-    enteredAmount: '',
-    enteredDate: ''
-  })
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
 
   function titleChangeHandler(event) {
     // setEnteredTitle(event.target.value)
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value,
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        enteredTitle: event.target.value,
+      };
     });
   }
 
   function amountChangeHandler(event) {
-    // setEnteredAmount(event.target.value)
-    setUserInput({
-      ...userInput,
-      enteredAmount: event.target.value,
+    // setEnteredAmount(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
+
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        enteredAmount: event.target.value,
+      };
     });
   }
 
   function dateChangeHandler(event) {
-    // setEnteredADate(event.target.value)
-    setUserInput({
-      ...userInput,
-      enteredDate: event.target.value,
+    // setEnteredADate(event.target.value);
+
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        enteredDate: event.target.value,
+      };
     });
   }
-
-  console.log(enteredAmount);
-  console.log(enteredTitle);
-  console.log(enteredDate);
 
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler}/>
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
