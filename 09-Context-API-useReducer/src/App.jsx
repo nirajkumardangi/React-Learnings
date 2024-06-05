133303fb147065346a1b3b40220db7b4f6df89f4
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-06-05 13:36:18 +0530

    Solution-1: to Solve Prop Drilling

--- 09-Context-API-useReducer/src/App.jsx
+++ 09-Context-API-useReducer/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
+import Product from './components/Product.jsx';
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -71,7 +72,15 @@ function App() {
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
       />
-      <Shop onAddItemToCart={handleAddItemToCart} />
+    <Shop>
+      <ul id="products">
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} onAddToCart={handleAddItemToCart} />
+          </li>
+        ))}
+      </ul>
+    </Shop>
     </>
   );
 }
