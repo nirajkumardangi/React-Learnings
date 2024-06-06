c55d0e2cace78b15a9b6e2649ea6f07dcee5d319
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-06-06 12:49:51 +0530

    Not All Side Effect Need useEffect

--- 10-Side-Effects-useEffect/src/App.jsx
+++ 10-Side-Effects-useEffect/src/App.jsx
@@ -41,6 +41,14 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    if (storedIds.IndexOf(id) === -1) {
+      localStorage.setItem(
+        "selectedPlaces",
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
