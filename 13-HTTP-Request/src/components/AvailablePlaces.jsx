217f8c60f33a1bbe39575c3b18ed5ec496b3a26e
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-06-10 15:25:10 +0530

    Extracting Code & Improving Code Structure

--- 13-HTTP-Request/src/components/AvailablePlaces.jsx
+++ 13-HTTP-Request/src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
 import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -13,16 +14,10 @@ export default function AvailablePlaces({ onSelectPlace }) {
       setIsFetching(true);
 
       try {
-        const response = await fetch("http://localhost:3000/places");
-        const resData = await response.json();
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch places");
-        }
-
+        const places = await fetchAvailablePlaces();
         navigator.geolocation.getCurrentPosition((position) => {
           const sortedPlace = sortPlacesByDistance(
-            resData.places,
+            places,
             position.coords.latitude,
             position.coords.longitude
           );
