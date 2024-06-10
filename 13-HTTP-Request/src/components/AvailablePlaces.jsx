58f7ea3866a0e6f9b839d5459d0744629878c40b
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-06-10 13:58:23 +0530

    Fetching Data by Using Async Await

--- 13-HTTP-Request/src/components/AvailablePlaces.jsx
+++ 13-HTTP-Request/src/components/AvailablePlaces.jsx
@@ -5,13 +5,13 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => {
-        setAvailablePlaces(resData.places);
-      });
+    async function fetchPlaces() {
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+    }
+
+    fetchPlaces();
   }, []);
 
   return (
--- 13-HTTP-Request/src/components/Places.jsx
+++ 13-HTTP-Request/src/components/Places.jsx
@@ -1,5 +1,4 @@
 export default function Places({ title, places, fallbackText, onSelectPlace }) {
-  console.log(places);
   return (
     <section className="places-category">
       <h2>{title}</h2>
