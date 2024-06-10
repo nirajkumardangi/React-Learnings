d13cccd270872bb456f9259ed182441aad50a549
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-06-10 13:49:06 +0530

    Sending HTTP Request to the Backend | Fetching Data | Using useEffect

--- 13-HTTP-Request/src/components/AvailablePlaces.jsx
+++ 13-HTTP-Request/src/components/AvailablePlaces.jsx
@@ -1,10 +1,23 @@
-import Places from './Places.jsx';
+import { useEffect, useState } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/places")
+      .then((response) => {
+        return response.json();
+      })
+      .then((resData) => {
+        setAvailablePlaces(resData.places);
+      });
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
