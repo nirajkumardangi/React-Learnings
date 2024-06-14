596e4379bce3ed7fb2e55acb390a169270b8b2e0
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-06-14 14:11:54 +0530

    Validating Input via Built-in Validation Props

--- 15-Form-Controls/src/App.jsx
+++ 15-Form-Controls/src/App.jsx
@@ -1,14 +1,12 @@
 import Header from './components/Header.jsx';
-import Login from './components/Login.jsx';
-// import Signup from './components/Signup.jsx';
-// import StateLogin from './components/StateLogin.jsx';
+import Signup from './components/Signup.jsx';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Login />
+        <Signup />
       </main>
     </>
   );
--- 15-Form-Controls/src/components/Signup.jsx
+++ 15-Form-Controls/src/components/Signup.jsx
@@ -18,13 +18,19 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email" required />
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            required
+            minLength={6}
+          />
         </div>
 
         <div className="control">
@@ -33,6 +39,8 @@ export default function Signup() {
             id="confirm-password"
             type="password"
             name="confirm-password"
+            required
+            minLength={6}
           />
         </div>
       </div>
@@ -42,18 +50,18 @@ export default function Signup() {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input type="text" id="first-name" name="first-name" required />
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input type="text" id="last-name" name="last-name" required />
         </div>
       </div>
 
       <div className="control">
         <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role">
+        <select id="role" name="role" required>
           <option value="student">Student</option>
           <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
@@ -92,8 +100,13 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
-          agree to the terms and conditions
+          <input
+            type="checkbox"
+            id="terms-and-conditions"
+            name="terms"
+            required
+          />
+          I agree to the terms and conditions
         </label>
       </div>
 
