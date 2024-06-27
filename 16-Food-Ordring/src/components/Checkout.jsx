7800c4702f9feb4b55a440d0c394aae0ca1cdc1b
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-06-27 20:48:04 +0530

    Sending A POST Request With Order

--- 16-Food-Ordring/src/components/Checkout.jsx
+++ 16-Food-Ordring/src/components/Checkout.jsx
@@ -24,6 +24,19 @@ export default function Checkout() {
 
     const fd = new FormData(event.target);
     const customerData = Object.fromEntries(fd.entries()); // {email: example@email.com}
+
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        order: {
+          items: items,
+          customer: customerData,
+        },
+      }),
+    });
   }
 
   return (
@@ -35,7 +48,8 @@ export default function Checkout() {
       <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-        <Input label="Full Name" type="text" id="full-name" />
+
+        <Input label="Full Name" type="text" id="name" />
         <Input label="E-Mail Address" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
         <div className="control-row">
