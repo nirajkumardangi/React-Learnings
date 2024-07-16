a3ab0e580f9d167bd10d76d4ce3e94094391f86a
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-07-16 12:08:33 +0530

    Aternative Way to Add Route

--- 20-React-Router-Basics/src/App.jsx
+++ 20-React-Router-Basics/src/App.jsx
@@ -1,12 +1,26 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 
 import HomePage from "./assets/pages/HomePage";
 import Products from "./assets/pages/Products";
 
-const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/products", element: <Products /> },
-]);
+const routerDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/products" element={<Products />} />
+  </Route>
+);
+
+const router = createBrowserRouter(routerDefinitions)
+
+// const router = createBrowserRouter([
+//   { path: "/", element: <HomePage /> },
+//   { path: "/products", element: <Products /> },
+// ]);
 
 function App() {
   return <RouterProvider router={router} />;
