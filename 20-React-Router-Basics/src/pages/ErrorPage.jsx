a3767c073d3ae58dac00c06c588c785cd728c418
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-07-17 07:49:17 +0530

    Creating Custom Error Page

---
+++ 20-React-Router-Basics/src/pages/ErrorPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ErrorPage() {
+  return (
+    <main>
+      <h1>This is a Custom Error Page</h1>
+    </main>
+  );
+}
--- 20-React-Router-Basics/src/pages/Root.jsx
+++ 20-React-Router-Basics/src/pages/Root.jsx
@@ -6,7 +6,7 @@ import classes from "./Root.module.css";
 export default function RootLayout() {
   return (
     <>
-      <MainNavigation />
+      <MainNavigation /> 
       <main className={classes.content}>
         <Outlet />
       </main>
