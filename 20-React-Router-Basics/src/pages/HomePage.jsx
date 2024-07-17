f07ea039230d74645b309a96d26202764349a938
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-07-17 08:18:28 +0530

    Navigating Programmatically (useNavigate)

--- 20-React-Router-Basics/src/pages/HomePage.jsx
+++ 20-React-Router-Basics/src/pages/HomePage.jsx
@@ -1,8 +1,16 @@
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate("/products");
+  }
+
   return (
     <>
       <h1>HomePage</h1>
+      <button onClick={navigateHandler}>Navigate</button>
     </>
   );
 }
