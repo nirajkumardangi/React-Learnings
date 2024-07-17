e77a68c40257ed4db13871932a256af40bf19cbb
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-07-17 16:03:05 +0530

    Data Fetching with a loader()

--- 21-Router-Advanced/frontend/src/App.jsx
+++ 21-Router-Advanced/frontend/src/App.jsx
@@ -54,7 +54,20 @@ const router = createBrowserRouter([
         path: "events",
         element: <EventsRootLayout />,
         children: [
-          { index: true, element: <EventsPage /> },
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: async () => {
+              const response = await fetch("http://localhost:8080/events");
+
+              if (!response.ok) {
+                //...
+              } else {
+                const resData = await response.json();
+                return resData.events;
+              }
+            },
+          },
           { path: ":eventId", element: <EventDetailPage /> },
           { path: "new", element: <NewEventPage /> },
           { path: ":eventId/edit", element: <EditEventPage /> },
--- 21-Router-Advanced/frontend/src/pages/Events.jsx
+++ 21-Router-Advanced/frontend/src/pages/Events.jsx
@@ -1,22 +1,28 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-const DUMMY_EVENTS = [
-  { id: "e1", title: "Some Event" },
-  { id: "e2", title: "Another Event" },
-  { id: "e3", title: "Last Event" },
-];
+import EventsList from "../components/EventsList";
 
 export default function EventsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchedEvents, setFetchedEvents] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchEvents() {
+      setIsLoading(true);
+      
+      setIsLoading(false);
+    }
+
+    fetchEvents();
+  }, []);
   return (
     <>
-      <h1>EventsPage</h1>
-      <ul>
-        {DUMMY_EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={event.id}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <div style={{ textAlign: "center" }}>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
     </>
   );
 }
