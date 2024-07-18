42af4eb467e3556556b9f493da36da3815148bef
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-07-18 12:13:21 +0530

    Returning Responses in loader()s

--- 21-Router-Advanced/frontend/src/pages/Events.jsx
+++ 21-Router-Advanced/frontend/src/pages/Events.jsx
@@ -3,19 +3,22 @@ import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 export default function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+  const events = data.events;
 
   return <EventsList events={events} />;
 }
 
-// define router loader function 
+// define router loader function
 export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
     //...
   } else {
-    const resData = await response.json();
-    return resData.events;
+    // const resData = await response.json();
+    // return resData.events;
+
+    return response; // because loader fn automatically resolve <promises>
   }
 }
