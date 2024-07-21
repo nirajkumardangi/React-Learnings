374b68ad20ba7a92fc5037e9f792f9716f8c820f
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-07-21 13:20:44 +0530

    Resolve Console Warnings!

--- 22-Authentication/frontend/src/components/AuthForm.jsx
+++ 22-Authentication/frontend/src/components/AuthForm.jsx
@@ -1,26 +1,38 @@
-import { Form, Link, useSearchParams } from 'react-router-dom';
+import { Form, Link, useSearchParams } from "react-router-dom";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 function AuthForm() {
- const [searchParams] = useSearchParams();
- const isLogin = searchParams.get('mode') === 'login';
+  const [searchParams] = useSearchParams();
+  const isLogin = searchParams.get("mode") === "login";
 
   return (
     <>
       <Form method="post" className={classes.form}>
-        <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
+        <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
         <p>
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" required />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            autoComplete="email"
+            required
+          />
         </p>
         <p>
           <label htmlFor="image">Password</label>
-          <input id="password" type="password" name="password" required />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            autoComplete="current-password"
+            required
+          />
         </p>
         <div className={classes.actions}>
-          <Link to={`?mode=${isLogin ? 'signup' : 'login'}`}>
-            {isLogin ? 'Create new user' : 'Login'}
+          <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
+            {isLogin ? "Create new user" : "Login"}
           </Link>
           <button>Save</button>
         </div>
