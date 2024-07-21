b806e86770c95b9b40a1c9dd132890cd198026ba
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-07-21 10:39:53 +0530

    Finished!

--- 21-Router-Advanced/frontend/src/pages/Home.jsx
+++ 21-Router-Advanced/frontend/src/pages/Home.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import PageContent from "../components/PageContent";
 
 export default function HomePage() {
   return (
-    <h1>HomePage</h1>
-  )
+    <PageContent title="Welcome!">
+      <p>Browse all our amazing events!</p>
+    </PageContent>
+  );
 }
