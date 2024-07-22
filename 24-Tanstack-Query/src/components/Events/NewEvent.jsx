f3ce3b8684e4a9ade13320b20ddbc7754c1dd2dc
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-07-22 17:14:25 +0530

    Changing Data with Mutation

--- 24-Tanstack-Query/src/components/Events/NewEvent.jsx
+++ 24-Tanstack-Query/src/components/Events/NewEvent.jsx
@@ -1,25 +1,38 @@
 import { Link, useNavigate } from 'react-router-dom';
+import { useMutation } from '@tanstack/react-query';
+import { createNewEvent } from '../../util/http.js';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import ErrorBlock from '../UI/ErrorBlock';
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: createNewEvent,
+  });
+
+  function handleSubmit(formData) {
+    mutate({ event: formData });
+  }
 
   return (
     <Modal onClose={() => navigate('../')}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        {isPending && 'Submitting...'}
+        {!isPending && (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        )}
       </EventForm>
+      {isError && <ErrorBlock title="Failed to create events" message={error.info?.message || 'Failed to create event. Please check your inputs and try again later.'} />}
     </Modal>
   );
 }
