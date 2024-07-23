c3c4590a919fa5d0b2f3bccb2640cb568dda2fe2
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-07-23 10:39:05 +0530

    Enhancing the App, Adding Modal on Deletion

--- 24-Tanstack-Query/src/components/Events/EventDetails.jsx
+++ 24-Tanstack-Query/src/components/Events/EventDetails.jsx
@@ -1,32 +1,49 @@
+import { useState } from 'react';
 import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
 import { useQuery, useMutation } from '@tanstack/react-query';
 
 import Header from '../Header.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 import { deleteEvent, fetchEvent, queryClient } from '../../util/http.js';
+import Modal from '../UI/Modal.jsx';
 
 export default function EventDetails() {
+  const [isDeleting, setIsDeleting] = useState(false);
+
   const { id } = useParams();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const { data, isPending, isError, error } = useQuery({
     queryKey: ['events', id],
     queryFn: ({ signal }) => fetchEvent({ id, signal }),
   });
 
-  const { mutate } = useMutation({
+  const {
+    mutate,
+    isPending: isPendingDeletion,
+    isError: isErrorDeleting,
+    error: deleteError,
+  } = useMutation({
     mutationFn: deleteEvent,
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ['events'],
-        refetchType: 'none'  // disabling automatic refetching after invalidations 
-      })
+        refetchType: 'none', // disabling automatic refetching after invalidations
+      });
       navigate('/events');
-    }
+    },
   });
 
+  function handleStartDeleting() {
+    setIsDeleting(true);
+  }
+
+  function handleStopDeleting() {
+    setIsDeleting(false);
+  }
+
   function handleDelete() {
-    mutate({id});
+    mutate({ id });
   }
 
   let content;
@@ -65,7 +82,7 @@ export default function EventDetails() {
         <header>
           <h1>{data.title}</h1>
           <nav>
-            <button onClick={handleDelete}>Delete</button>
+            <button onClick={handleStartDeleting}>Delete</button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
@@ -87,6 +104,37 @@ export default function EventDetails() {
 
   return (
     <>
+      {isDeleting && (
+        <Modal onClose={handleStopDeleting}>
+          <h2>Are you sure?</h2>
+          <p>
+            Do you really want to delete this events? This action cannot be
+            undone
+          </p>
+          <div className="form-actions">
+            {isPendingDeletion && <p>Deleting, please wait...</p>}
+            {!isPendingDeletion && (
+              <>
+                <button onClick={handleStopDeleting} className="button-text">
+                  Cancel
+                </button>
+                <button onClick={handleDelete} className="button">
+                  Delete
+                </button>
+              </>
+            )}
+          </div>
+          {isErrorDeleting && (
+            <ErrorBlock
+              title="Failed to delete event"
+              message={
+                deleteError.info?.message ||
+                'Failed to delete event, please try again later'
+              }
+            />
+          )}
+        </Modal>
+      )}
       <Outlet />
       <Header>
         <Link to="/events" className="nav-item">
