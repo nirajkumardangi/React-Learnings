2623b4e0ba1f008aebc45aab294102078ad9e73f
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-07-24 11:20:06 +0530

    Adding & Handling Error Page Component

---
+++ 26-NextJs-Foodies-App/app/meals/error.js
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Error({error}) {
+  return (
+    <main className="error">
+    <h1>An error occurred!</h1>
+    <p>Failed to fetch meal data, Please try agin later.</p>
+    </main>
+  )
+}
--- 26-NextJs-Foodies-App/lib/meals.js
+++ 26-NextJs-Foodies-App/lib/meals.js
@@ -4,5 +4,7 @@ const db = sql('meals.db');
 
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 2000));  // for extra delay
+
+  throw new Error('Loading Meals failed!');
   return db.prepare('SELECT * FROM meals').all();
 }
