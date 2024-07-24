d3a9eb8b7bff4af1c403562fb3f0b3167b2cada0
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-07-24 14:45:27 +0530

    Adding Server Side Input Validation

--- 26-NextJs-Foodies-App/lib/actions.js
+++ 26-NextJs-Foodies-App/lib/actions.js
@@ -4,6 +4,10 @@ import { redirect } from 'next/navigation';
 
 import { saveMeal } from './meals';
 
+function isInvalidText(text) {
+  return !text || text.trim() === '';
+}
+
 export async function shareMeal(formData) {
   const meal = {
     title: formData.get('title'),
@@ -13,6 +17,19 @@ export async function shareMeal(formData) {
     creator: formData.get('name'),
     creator_email: formData.get('email'),
   };
+
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(meal.instructions) ||
+    isInvalidText(meal.creator) ||
+    isInvalidText(meal.creator_email) ||
+    !meal.creator_email.include('@') ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    throw new Error('Invalid Input');
+  }
 
   await saveMeal(meal);
   redirect('/meals');
