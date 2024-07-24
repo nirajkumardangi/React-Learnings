c6189761ee58818fc64a348009f3f0b576a284d4
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-07-24 13:24:12 +0530

    Creating a Slug & Sanitizing input for XSS Protection

--- 26-NextJs-Foodies-App/lib/meals.js
+++ 26-NextJs-Foodies-App/lib/meals.js
@@ -1,9 +1,11 @@
 import sql from 'better-sqlite3';
+import slugify from 'slugify';
+import xss from 'xss';
 
 const db = sql('meals.db');
 
 export async function getMeals() {
-  await new Promise((resolve) => setTimeout(resolve, 2000));  // for extra delay
+  await new Promise((resolve) => setTimeout(resolve, 2000)); // for extra delay
 
   // throw new Error('Loading Meals failed!');
   return db.prepare('SELECT * FROM meals').all();
@@ -11,4 +13,9 @@ export async function getMeals() {
 
 export function getMeal(slug) {
   return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+}
+
+export function saveMeal(meal) {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
 }
