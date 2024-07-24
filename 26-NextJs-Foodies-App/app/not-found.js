a586dc6dd90eb28687884dbf15887b567fd937f6
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-07-24 11:24:20 +0530

    Handling "Not Found" State

---
+++ 26-NextJs-Foodies-App/app/not-found.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not found</h1>
+      <p>Unfortunately, we could not find the requested page or resources</p>
+    </main>
+  );
+}
--- 26-NextJs-Foodies-App/lib/meals.js
+++ 26-NextJs-Foodies-App/lib/meals.js
@@ -5,6 +5,6 @@ const db = sql('meals.db');
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 2000));  // for extra delay
 
-  throw new Error('Loading Meals failed!');
+  // throw new Error('Loading Meals failed!');
   return db.prepare('SELECT * FROM meals').all();
 }
