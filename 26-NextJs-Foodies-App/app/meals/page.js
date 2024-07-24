725af578b949fa98337c81fbf786610e6a51ef0b
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-07-24 11:11:22 +0530

    Adding Loading Indicator & Using Suspense

--- 26-NextJs-Foodies-App/app/meals/page.js
+++ 26-NextJs-Foodies-App/app/meals/page.js
@@ -1,11 +1,17 @@
+import { Suspense } from 'react';
 import Link from 'next/link';
+
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
 
-export default async function MealsPage() {
+async function Meals() {
   const meals = await getMeals();
 
+  return <MealsGrid meals={meals} />;
+}
+
+export default async function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -21,7 +27,9 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={classes.loading}>Fetching Meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
