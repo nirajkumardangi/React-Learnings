24b1d0204dbfb10ead7f57daf6f87bca9b914582
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-07-24 13:05:42 +0530

    Using Server Actions for Handling Form Submissions

--- 26-NextJs-Foodies-App/components/meals/image-picker.js
+++ 26-NextJs-Foodies-App/components/meals/image-picker.js
@@ -18,6 +18,7 @@ export default function ImagePicker({ label, name }) {
 
     if (!file) {
       setPickedImage(null);
+      return;
     }
 
     const fileReader = new FileReader();
@@ -51,6 +52,7 @@ export default function ImagePicker({ label, name }) {
           name={name}
           ref={imageInput}
           onChange={handleImageChange}
+          required
         />
         <button
           className={classes.button}
