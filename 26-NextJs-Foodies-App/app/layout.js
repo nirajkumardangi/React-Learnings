d6e43ee3fffc444c8729dcaa932e6f0fa257e64c
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-07-24 07:20:22 +0530

    Solve Console Warning

--- 26-NextJs-Foodies-App/app/layout.js
+++ 26-NextJs-Foodies-App/app/layout.js
@@ -9,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body cz-shortcut-listen="true">
         <div className="header-background">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
             <defs>
