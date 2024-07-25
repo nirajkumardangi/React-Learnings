2d5fa8973eaaa9d29edbd20bc2ec88ae4c1d1638
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-07-25 11:07:28 +0530

    Animating Shared Element

--- 27-Animating-React-App/src/components/ChallengeTabs.jsx
+++ 27-Animating-React-App/src/components/ChallengeTabs.jsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import Badge from './Badge.jsx';
 
 function Tab({ isSelected, onSelect, badgeCaption, children }) {
@@ -10,7 +11,9 @@ function Tab({ isSelected, onSelect, badgeCaption, children }) {
         {children}
         <Badge caption={badgeCaption}></Badge>
       </button>
-      {isSelected && <div className="active-tab-indicator" />}
+      {isSelected && (
+        <motion.div layoutId="tab-indicator" className="active-tab-indicator" />
+      )}
     </li>
   );
 }
