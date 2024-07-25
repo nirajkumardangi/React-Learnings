1d2cd624e7260ddb1da31273e132b189c9be0460
Niraj Kumar Dangi <niraj8825224435@gmail.com>
2024-07-25 11:12:06 +0530

    Re-triggering Animations via Key

--- 27-Animating-React-App/src/components/Badge.jsx
+++ 27-Animating-React-App/src/components/Badge.jsx
@@ -1,3 +1,9 @@
+import { motion } from 'framer-motion';
+
 export default function Badge({ caption }) {
-  return <span className="badge">{caption}</span>;
+  return (
+    <motion.span animate={{ scale: [1, 1.3, 1] }} className="badge">
+      {caption}
+    </motion.span>
+  );
 }
